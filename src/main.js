38d108f5e3842e23380cc3b5d7157d37b869a345
Cristhian <csanchezm@unsa.edu.pe>
2021-11-11 22:33:06 -0500

    frist commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
+import VueAxios from "vue-axios";
 Vue.config.productionTip = false;
-
+axios.defaults.baseURL = "http://localhost:80/miscosas/wp-json/";
+Vue.use(VueAxios, axios);
 new Vue({
   router,
   store,
+  vuetify,
   render: (h) => h(App),
 }).$mount("#app");
